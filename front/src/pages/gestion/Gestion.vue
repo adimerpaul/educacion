b478bf319e9152f0b109cf2d34473c0f3c221ac5
adimer <adimer101@gmail.com>
2024-11-28 18:59:15 -0400

    Actulizado para boton de impresion de formualrio 1

--- front/src/pages/gestion/Gestion.vue
+++ front/src/pages/gestion/Gestion.vue
@@ -13,6 +13,7 @@
           <q-btn-group flat>
             <q-btn color="primary" icon="edit" @click="periodoEdit(props.row)" dense size="10px" />
             <q-btn color="negative" icon="delete" @click="periodoDelete(props.row.id)" dense size="10px" />
+            <q-btn color="primary" icon="print" @click="imprimirf1(props.row)" dense size="10px" />
           </q-btn-group>
         </q-td>
       </template>
@@ -25,9 +26,9 @@
       </template>
     </q-table>
     <q-dialog v-model="periodoDialog" persistent>
-      <q-card>
+      <q-card style="min-width: 70vw">
         <q-card-section class="q-pb-none row items-center">
-          <div>
+          <div class="text-subtitle2">
             {{ actionPeriodo }} periodo
           </div>
           <q-space />
@@ -35,10 +36,95 @@
         </q-card-section>
         <q-card-section class="q-pt-none">
           <q-form @submit="periodo.id ? periodoPut() : periodoPost()">
-<!--            <q-input v-model="periodo.periodo" label="Periodo" outlined dense />-->
-            <q-select v-model="periodo.periodo" label="Periodo" outlined dense :options="peridosSelect" />
-            <q-input v-model="periodo.fecha_inicio" label="Fecha inicio" type="date" outlined dense />
-            <q-input v-model="periodo.fecha_fin" label="Fecha fin" type="date" outlined dense />
+            <q-card flat bordered class="q-pa-md">
+            <div class="row">
+              <div class="col-12 text-subtitle2">
+                Gestión
+              </div>
+              <div class="col-12 col-md-4">
+                <q-select v-model="periodo.periodo" label="Periodo" outlined dense :options="peridosSelect"  />
+              </div>
+              <div class="col-12 col-md-4">
+                <q-input v-model="periodo.fechaInicio" label="Fecha inicio" type="date" outlined dense  />
+              </div>
+              <div class="col-12 col-md-4">
+                <q-input v-model="periodo.fechaFin" label="Fecha fin" type="date" outlined dense  />
+              </div>
+            </div>
+            </q-card>
+<!--            FORMULARIO Nro. 1-->
+            <q-card flat bordered class="q-pa-md q-mt-md">
+              <div class="row">
+                <div class="col-12 text-subtitle2">
+                  Formulario Nro. 1
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.accionInstitucional" label="Acción institucional" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.accionCortoPlazo" label="Acción a corto plazo" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.resultadoEsperado1" label="Resultado esperado 1" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.resultadoEsperado2" label="Resultado esperado 2" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input v-model="periodo.totalPresupuesto" label="Total presupuesto" outlined dense />
+                </div>
+              </div>
+            </q-card>
+<!--            FORMULARIO Nro. 2-->
+            <q-card flat bordered class="q-pa-md q-mt-md">
+              <div class="row">
+                <div class="col-12 text-subtitle2">
+                  Formulario Nro. 2
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.accionCortoPlazo1" label="Acción a corto plazo 1" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.accionCortoPlazo2" label="Acción a corto plazo 2" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.accionCortoPlazo3" label="Acción a corto plazo 3" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.resultadoEsperadoGestion1" label="Resultado esperado gestión 1" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.resultadoEsperadoGestion2" label="Resultado esperado gestión 2" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.resultadoEsperadoGestion3" label="Resultado esperado gestión 3" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.resultadoEsperadoGestion4" label="Resultado esperado gestión 4" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input type="textarea" v-model="periodo.resultadoEsperadoGestion5" label="Resultado esperado gestión 5" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input v-model="periodo.fechaPrevistaInicio1" label="Fecha prevista inicio 1" type="date" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input v-model="periodo.fechaPrevistaInicio2" label="Fecha prevista inicio 2" type="date" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input v-model="periodo.fechaPrevistaInicio3" label="Fecha prevista inicio 3" type="date" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input v-model="periodo.fechaPrevistaFinalizacion1" label="Fecha prevista finalización 1" type="date" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input v-model="periodo.fechaPrevistaFinalizacion2" label="Fecha prevista finalización 2" type="date" outlined dense />
+                </div>
+                <div class="col-12 col-md-4">
+                  <q-input v-model="periodo.fechaPrevistaFinalizacion3" label="Fecha prevista finalización 3" type="date" outlined dense />
+                </div>
+              </div>
+            </q-card>
             <div>
               <q-btn color="negative" label="Cancelar" @click="periodoDialog = false" no-caps :loading="loading" />
               <q-btn color="primary" label="Guardar" type="submit" no-caps :loading="loading" class="q-ml-sm" />
@@ -66,8 +152,8 @@ export default {
         // { name: 'id', label: 'ID', align: 'left', field: 'id', sortable: true },
         { name: 'periodo', label: 'Periodo', align: 'left', field: 'periodo', sortable: true },
         { name: 'estado', label: 'Estado', align: 'left', field: 'estado', sortable: true },
-        { name: 'fecha_inicio', label: 'Fecha inicio', align: 'left', field: 'fecha_inicio', sortable: true },
-        { name: 'fecha_fin', label: 'Fecha fin', align: 'left', field: 'fecha_fin', sortable: true },
+        { name: 'fechaInicio', label: 'Fecha inicio', align: 'left', field: 'fechaInicio', sortable: true },
+        { name: 'fechaFin', label: 'Fecha fin', align: 'left', field: 'fechaFin', sortable: true },
       ],
       peridosSelect: []
     }
@@ -82,9 +168,50 @@ export default {
   methods: {
     newPeriodo() {
       this.periodo = {
+      //   $table->integer('periodo');
+      // $table->date('fechaInicio');
+      // $table->date('fechaFin');
+      // $table->string('accionInstitucional')->nullable();
+      // $table->string('accionCortoPlazo')->nullable();
+      // $table->string('resultadoEsperado1')->nullable();
+      // $table->string('resultadoEsperado2')->nullable();
+      // $table->string('totalPresupuesto')->nullable();
+      // $table->string('accionCortoPlazo1')->nullable();
+      // $table->string('accionCortoPlazo2')->nullable();
+      // $table->string('accionCortoPlazo3')->nullable();
+      // $table->string('resultadoEsperadoGestion1')->nullable();
+      // $table->string('resultadoEsperadoGestion2')->nullable();
+      // $table->string('resultadoEsperadoGestion3')->nullable();
+      // $table->string('resultadoEsperadoGestion4')->nullable();
+      // $table->string('resultadoEsperadoGestion5')->nullable();
+      // $table->string('fechaPrevistaInicio1')->nullable();
+      // $table->string('fechaPrevistaInicio2')->nullable();
+      // $table->string('fechaPrevistaInicio3')->nullable();
+      // $table->string('fechaPrevistaFinalizacion1')->nullable();
+      // $table->string('fechaPrevistaFinalizacion2')->nullable();
+      // $table->string('fechaPrevistaFinalizacion3')->nullable();
         periodo: moment().year(),
-        fecha_inicio: moment().format('YYYY-MM-DD'),
-        fecha_fin: moment().format('YYYY-MM-DD')
+        fechaInicio: moment().format('YYYY-MM-DD'),
+        fechaFin: moment().format('YYYY-MM-DD'),
+        accionInstitucional: 'Lograr la participación, corresponsabilidad social comunitaria y armonizada de todos los actores involucrados en la educación en el departamento.',
+        accionCortoPlazo: 'Universalizar el Modelo Educativo Socio-Comunitario Productivo (MESCP) y Lograr la participación, corresponsabilidad social comunitaria y armonizada de todos los actores involucrados en la educación en el departamento de Oruro. en la gestión 2020, enmarcado en la cpe, ley n°1178, ley n°070 de educación "avelino siñani - elizardo pérez", d.s. n°813, normas generales para la gestión educativa y normas conexas.',
+        resultadoEsperado1: 'Se incentivó la participación activa de padres y madres de familia, y organizaciones sociales en la implementación de los documentos curriculares.',
+        resultadoEsperado2: 'Se incentivó la apropiación y funcionamiento de los consejos Educativos.',
+        totalPresupuesto: 2186420,
+        accionCortoPlazo1: 'Universalizar el Modelo Educativo Socio-Comunitario Productivo (MESCP) y Lograr la participación, corresponsabilidad social comunitaria y armonizada de todos los actores involucrados en la educación en el departamento de Oruro. en la gestión 2020, enmarcado en la cpe, ley n°1178, ley n°070 de educación "avelino siñani - elizardo pérez", d.s. n°813, normas generales para la gestión educativa y normas conexas.',
+        accionCortoPlazo2: 'Universalizar el Modelo Educativo Socio-Comunitario Productivo (MESCP) y Lograr la participación, corresponsabilidad social comunitaria y armonizada de todos los actores involucrados en la educación en el departamento de Oruro. en la gestión 2020, enmarcado en la cpe, ley n°1178, ley n°070 de educación "avelino siñani - elizardo pérez", d.s. n°813, normas generales para la gestión educativa y normas conexas.',
+        accionCortoPlazo3: 'Universalizar el Modelo Educativo Socio-Comunitario Productivo (MESCP) y Lograr la participación, corresponsabilidad social comunitaria y armonizada de todos los actores involucrados en la educación en el departamento de Oruro. en la gestión 2020, enmarcado en la cpe, ley n°1178, ley n°070 de educación "avelino siñani - elizardo pérez", d.s. n°813, normas generales para la gestión educativa y normas conexas.',
+        resultadoEsperadoGestion1: 'Se incentivó la participación activa de padres y madres de familia, y organizaciones sociales en la implementación de los documentos curriculares.',
+        resultadoEsperadoGestion2: 'Se incentivó la apropiación y funcionamiento de los consejos Educativos.',
+        resultadoEsperadoGestion3: 'Se incentivó la participación activa de padres y madres de familia, y organizaciones sociales en la implementación de los documentos curriculares.',
+        resultadoEsperadoGestion4: 'Se incentivó la apropiación y funcionamiento de los consejos Educativos.',
+        resultadoEsperadoGestion5: 'Se incentivó la participación activa de padres y madres de familia, y organizaciones sociales en la implementación de los documentos curriculares.',
+        fechaPrevistaInicio1: '2020-01-01',
+        fechaPrevistaInicio2: '2020-01-01',
+        fechaPrevistaInicio3: '2020-01-01',
+        fechaPrevistaFinalizacion1: '2020-12-31',
+        fechaPrevistaFinalizacion2: '2020-12-31',
+        fechaPrevistaFinalizacion3: '2020-12-31',
       }
       this.actionPeriodo = 'Nuevo'
       this.periodoDialog = true
@@ -137,6 +264,10 @@ export default {
       this.periodo = { ...periodo }
       this.actionPeriodo = 'Editar'
       this.periodoDialog = true
+    },
+    imprimirf1(periodo) {
+      const url = this.$url + 'formulario1/' + periodo.id
+      window.open(url, '_blank')
     },
     periodoDelete(id) {
       this.$alert.dialog('¿Desea eliminar el periodo?')
